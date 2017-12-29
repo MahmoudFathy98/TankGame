2905fca7a7bdbe74358894e1afddd3989fb428b4
Mahmoud Ahmed Fathy <32363196+MahmoudFathy98@users.noreply.github.com>
2017-12-29 22:10:21 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package ch8threads;
 
@@ -17,13 +12,10 @@ import javax.swing.JLabel;
 
 /**
  *
- * @author ASUS
+ * @author MahmoudFathy
  */
 public class Ch8Threads {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         
